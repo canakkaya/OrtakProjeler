f41ee9051caaa02fa5ee99b159e0816ce06a2618
Erdi Bleda <erdiata@gmail.com>
2014-10-17 16:49:14 +0300

    Proje sablonu

---
+++ transpoz/transpoz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// Tarih: 14 Ekim 2014
+
+// Projede amaciniz a matrisinin transpozesini almak:
+//
+// Istenenler:
+// 1) Matrris elemanlari kullanici tarafindan girilecek
+// 2) Matrisin transpozesi alinacak baska bir matrise esitlenecek (mesela b matrisi)
+// 3) b matrisi bir matris formatinda ekrana yazdirilacak
+// 
+// Size sablon olarak verilen kisimlari degistirmeden kodunuzu yazin
+
+int main() {
+    int a[5][8];
+    int b[ ][ ];    // <=== matris boyunu girin
+
+
+
+    return 0;
+}
