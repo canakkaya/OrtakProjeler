ddd3dc3e66f5e327d0768861ea993a502c2af552
Erdi A. Bleda <erdiata@gmail.com>
2014-10-18 23:21:04 +0300

    biraz styling biraz ekstra parantezlerin kalkisi

--- transpoz/transpoz.c
+++ transpoz/transpoz.c
@@ -14,31 +14,29 @@ int main(){
 
      printf("40 adet matris elemanini gir\n");   					 // a[i][j] matrisi elle yazilacak sekilde atandi
      for(i=0;i<5;i++){
-     for(j=0;j<8;j++){
-     scanf("%d",&a[i][j]);
-     
+        for(j=0;j<8;j++){
+            scanf("%d",&a[i][j]);
+        }
      }
-	 }
 	 
     printf("a matrisi");
     
     for(i=0;i<5;i++){       			     // 5 tane satirin degeri girildi
-	printf("\n");               	    	// satirlari alt alta gostermek icin.Her 1 islemlik satir islemini tamamladiktan sonra alt satira gec
-	for(j=0;j<8;j++){   			       // i ye karsilik j degerleri atandi 
-	printf("%3d",a[i][j]);               //goruntu net olmasi icin elemanlar arasi 3 bosluk birakildi
-	b[j][i]=a[i][j];                     //kumenin son islemi olarak " b nin j satirina a nin i satiri atandi" "b nin i sutununa a nin j sutunu atandi" ve transpse edildi
+	    printf("\n");               	    	// satirlari alt alta gostermek icin.Her 1 islemlik satir islemini tamamladiktan sonra alt satira gec
+	    for(j=0;j<8;j++){   			       // i ye karsilik j degerleri atandi 
+	        printf("%3d",a[i][j]);               //goruntu net olmasi icin elemanlar arasi 3 bosluk birakildi
+	        b[j][i]=a[i][j];                     //kumenin son islemi olarak " b nin j satirina a nin i satiri atandi" "b nin i sutununa a nin j sutunu atandi" ve transpse edildi
 							        	//artik isleme bu kumede devam edemeyiz b matrisine i ve j degeri atamak icin asagida ayri kume acildi
-	}
+	    }
 	}
 
 	printf("\n");				
 	printf("transpose\n");			    	  //  transpose edilmis hali asagida gosterir
 	for(j=0;j<8;j++){		    			 // transpose edilen satirlari diziyorum
-    printf("\n");{      					//satirlari alt alta gosterir her bi j satir islemi biter ardindan alt satira iner
-	for(i=0;i<5;i++){  					   //i sutunlari
-	printf("%3d",b[j][i]);		  	 	  // hayirli olsun ...
-   	}
-	}
+        printf("\n");      					//satirlari alt alta gosterir her bi j satir islemi biter ardindan alt satira iner
+	    for(i=0;i<5;i++){  					   //i sutunlari
+	        printf("%3d",b[j][i]);		  	 	  // hayirli olsun ...
+	    }
 	}
 	return 0;
 	
